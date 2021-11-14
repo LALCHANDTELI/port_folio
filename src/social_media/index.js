ece9dfdc8b625dd021639dff99008df8935ee37b
lal chand teli <42927241+LALCHANDTELI@users.noreply.github.com>
2021-11-14 18:48:47 +0530

    Update index.js

--- src/social_media/index.js
+++ src/social_media/index.js
@@ -1,7 +1,7 @@
 export const social_media=()=>{
     return(
-        <div class="col-sm-12">
-            <div class="row fourth_row">
+        <div class="col-sm-12 ">
+            <div class="row fourth_row ">
               <div class="col-md-4">
                 <i class="fab fa-linkedin fa-2x"></i>
                 <span>117</span> professional connections
